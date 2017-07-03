5c0b3315e5300b2a0b535c74706e7fffab5de65c
Arthur Rozenberg <rozenarthur@gmail.com>
2017-07-03 16:04:30 -0400

    add clear all reminders at once feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { addReminder, deleteReminder } from '../actions';
+import { addReminder, deleteReminder, clearReminders } from '../actions';
 import moment from 'moment';
 
 class App extends Component{
@@ -73,6 +73,9 @@ class App extends Component{
                             Add Reminder
                     </button>
                 </div>
+                <div className="btn btn-danger"
+                     onClick={() => this.props.clearReminders()}
+                >Clear All Reminders</div>
                 { this.renderReminder()}
             </div>
         )
@@ -85,4 +88,4 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
